1aa38b1fe4ad2f91644d0005d19ceec365dc76ca
Son of Adam <101136948+SonsOfAdam@users.noreply.github.com>
2024-01-18 21:19:45 +0100

    0/price update

--- actions/verify-transaction.ts
+++ actions/verify-transaction.ts
@@ -7,43 +7,41 @@ var paystack = require('paystack')('sk_live_7803c444e118e885450630864d6430bade83
 export const verifyTransaction = async (
   reference: string,
   courseId: string,
-  price: number, // Add a parameter for the course price
+  price: number,
 ): Promise<{ success: boolean } | any> => {
   try {
     const verifyResponse = await paystack.transaction.verify(reference);
 
-    if (verifyResponse.data.status === 'success') {
-      const user = await currentUser();
-      if (!user || !user.id || !courseId) {
-        return new NextResponse(`Webhook Error: Missing metadata`, { status: 400 });
-      }
-
-      if (price === 0) {
-        // Handle free course logic (e.g., grant access without payment)
-        await db.purchase.create({
-          data: {
-            courseId: courseId,
-            userId: user.id,
-          },
-        });
-
-        return { success: true };
-      } else {
-        // Handle paid course logic
-        // You might want to process the payment here
-
-        await db.purchase.create({
-          data: {
-            courseId: courseId,
-            userId: user.id,
-          },
-        });
-
-        return { success: true };
-      }
+    if (verifyResponse.data.status !== 'success') {
+      return new NextResponse(`Webhook Error: Verification failed`, { status: 400 });
     }
 
-    return new NextResponse(`Webhook Error: Verification failed`, { status: 400 });
+    const user = await currentUser();
+    if (!user || !user.id || !courseId) {
+      return new NextResponse(`Webhook Error: Missing metadata`, { status: 400 });
+    }
+
+    if (price === 0) {
+      // Handle free course logic
+      await db.purchase.create({
+        data: {
+          courseId: courseId,
+          userId: user.id,
+        },
+      });
+
+      return { success: true };
+    }
+
+    // Handle paid course logic (add payment processing logic here)
+    await db.purchase.create({
+      data: {
+        courseId: courseId,
+        userId: user.id,
+      },
+    });
+
+    return { success: true };
   } catch (error) {
     console.log("[VERIFY_TXN]", error);
     return 0;
